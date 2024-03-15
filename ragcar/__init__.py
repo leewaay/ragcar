dd0056fac884eda019728ae7809a58961f9bad45
leewaay <wonsuklee7020@gmail.com>
2024-03-15 16:08:14 +0900

    v0.1.1 Modify install_requires

--- ragcar/__init__.py
+++ ragcar/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from ragcar.utils.prompt_template import PromptTemplate
 from ragcar.ragcar import Ragcar
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ragcar', 
-    version='0.1.0', 
+    version='0.1.1', 
     url='https://github.com/leewaay/ragcar.git', 
     author='Wonseok Lee', 
     author_email='wonsuklee7020@gmail.com', 
